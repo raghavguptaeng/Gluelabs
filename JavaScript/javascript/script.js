595a72035cf6462f9723f36eba23f9d2d6d6425a
Raghav Gupta <E19CSE258@bennett.edu.in>
2022-03-23 02:39:53 +0530

    bug fixed

---
+++ JavaScript/javascript/script.js
@@ -0,0 +1,52 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email')
+const submit = document.getElementById('sumbit');
+const password = document.getElementById('password')
+const err = document.querySelector('.msg');
+
+const email_regex = /^(.+)@(.+)$/;
+const password_regex = /^[a-zA-Z]+$/;
+submit.addEventListener('click',validate);
+
+function validate(e){
+    e.preventDefault();
+    console.log(err);
+    if(email.value==='' || name.value===''  ||  !checkPassword(password.value) ){
+        err.classList.add('error');
+        err.innerText = 'Invalid Input';
+        console.log(password.value)
+        setTimeout(() => {
+            err.classList.remove('error');
+            err.innerText = '';
+        }, 3000);
+    }
+    else{
+        console.log('ok');
+        console.log(checkPassword(password.value));
+    }
+}
+
+function checkPassword(str){
+    var cap = 0;
+    var sm = 0;
+    var num = 0;
+    for(var i=0 ; i<str.length ; ++i){
+        var code = str.charCodeAt(i);
+        if(code>=65 && code<=90 ){
+            sm++;
+        }
+        else if(code>=97 && code<=122){
+            cap++;
+        }
+        else if(code>=48 && code <=57){
+            num++;
+        }
+    }
+    console.log(cap);
+    console.log(sm);
+    console.log(num);
+    if(cap>0 && sm>0 && num>0 && str.length >=8){
+        return true;
+    }
+    return false;
+}
