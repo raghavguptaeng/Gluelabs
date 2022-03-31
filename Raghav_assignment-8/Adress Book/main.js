742e77e842b533d718f3f0ffcc5d7510eecc0dd6
Raghav Gupta <E19CSE258@bennett.edu.in>
2022-03-31 13:52:41 +0530

    Address book bug fixedf

--- Raghav_assignment-8/Adress Book/main.js
+++ Raghav_assignment-8/Adress Book/main.js
@@ -1,12 +1,13 @@
 document.getElementById('filterInput').addEventListener('keyup', e=>{
     let filter = e.target.value.toUpperCase();
     let li = document.querySelectorAll('.collection-item');
-    for(let i = 0; i < li.length; i++){
-        let a = li[i].getElementsByTagName('a')[0];
-        if(a.innerHTML.toUpperCase().indexOf(filter) > -1){
-            li[i].style.display = '';
-        } else {
-            li[i].style.display = 'none';
-        }
-    }
+    console.log(li);
+    li.forEach(item=>{
+        let a = item.querySelector('a');
+        console.log(a.innerText); 
+        item.style.display = a.innerText.toUpperCase().startsWith(filter)?'block':'none';
+        document.querySelectorAll('.collection-header').forEach(head=>{
+            head.style.display = head.querySelector('h5').innerText.toUpperCase().startsWith(filter)?'block':'none';
+        });
+    });
 });
