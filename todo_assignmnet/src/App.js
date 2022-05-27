71c6337a8f4dcfa89678b4cd2d521a06ec23b63b
Raghav Gupta <E19CSE258@bennett.edu.in>
2022-05-27 11:20:25 +0530

    Input Box UI DOne

--- todo_assignmnet/src/App.js
+++ todo_assignmnet/src/App.js
@@ -1,9 +1,11 @@
+import MainBody from "./components/MainBody";
 import SideNav from "./components/sideNav";
 
 function App() {
   return (
     <div className="App">
       <SideNav />
+      <MainBody />
     </div>
   );
 }
---
+++ todo_assignmnet/src/components/MainBody.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class MainBody extends Component {
+    render() {
+        return (
+            <div className='mainBody'>
+                <div className='mainBody__container'>
+                    <div className='mainBody__container__title'>
+                        <h1>todos</h1>
+                    </div>
+                    <div className="inputTodo">
+                        <input type="text" placeholder="What needs to be done?" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default MainBody;
